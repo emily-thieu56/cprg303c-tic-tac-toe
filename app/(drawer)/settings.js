42a8de4f0bf8740886938da74198a257408cbba9
emily-thieu56 <152931248+emily-thieu56@users.noreply.github.com>
2024-12-03 19:43:27 -0700

    Creating color scheme selection

--- app/(drawer)/settings.js
+++ app/(drawer)/settings.js
@@ -1,12 +1,56 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { RadioButton } from 'react-native-paper';
 
 const settings = () => {
+  const [checked, setChecked] = React.useState('first');
   return (
     <View>
-      <Text>settings</Text>
+      <Text>Settings</Text>
+
+      <h2>Color Scheme</h2>
+      {/*Select color scheme */}
+      <RadioButton
+        value="first"
+        status={ checked === 'first' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('first')}
+      />
+      <RadioButton
+        value="second"
+        status={ checked === 'second' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('second')}
+      />
+
+      <RadioButton
+        value="third"
+        status={ checked === 'third' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('third')}
+      />
+      <RadioButton
+        value="fourth"
+        status={ checked === 'fourth' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('fourth')}
+      />
+
+      <RadioButton
+        value="fifth"
+        status={ checked === 'fifth' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('fifth')}
+      />
+      <RadioButton
+        value="sixth"
+        status={ checked === 'sixth' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('sixth')}
+      />
+
+      <RadioButton
+        value="seventh"
+        status={ checked === 'seventh' ? 'checked' : 'unchecked' }
+        onPress={() => setChecked('seventh')}
+      />
     </View>
   )
 }
 
+const styles = StyleSheet.create({})
 export default settings
